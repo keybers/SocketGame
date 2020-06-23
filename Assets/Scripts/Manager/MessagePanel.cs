e5650284ab596e791be02b110c66ac8be1017d68
Keyber <keyberboy@qq.com>
2020-06-23 08:37:16 +0800

    写了UI信息提示脚本

---
+++ Assets/Scripts/Manager/MessagePanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessagePanel : BasePanel
+{
+    public Text text;
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        text.CrossFadeAlpha(0, 0.1f, false);
+    }
+
+    public void ShowMessage(string message)
+    {
+        text.text = message;
+        text.CrossFadeAlpha(1, 1f, false);
+        Invoke("HideText",1);
+    }
+
+    private void HideText()
+    {
+        text.CrossFadeAlpha(0, 1f, false);
+    }
+}
